2486d08a3a9180a627ad78867a676b5357a79158
gordanchen <126982681+gordanchen@users.noreply.github.com>
2026-08-10 04:52:48 +0800

    Update API_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.GQ_CONFIG = {
   // 部署 Apps Script 後，把 /exec 網址貼在這裡。
-  API_URL: 'https://script.google.com/macros/s/AKfycbzqW8mMXu4Bh9Cut-QVdwREBpwJW6fLdhb5V8vBV9zgc7_MROiEmynuarJzO0otCgje/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwVJShrBV42X8nD775xFf9rdZabpNEAHy2ZeUJApqbi_8tQiTaDoHKJ4JXv6P4yNmE/exec',
   APP_NAME: '恭權薪資統計系統',
 
   // 通知服務 App ID（公開識別碼，可以放在前端）。
