468445a07738d52e174a94879d07971629efd0f1
gordanchen <126982681+gordanchen@users.noreply.github.com>
2026-08-10 05:19:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='gongquan-salary-v2-notifications';
+const CACHE='gongquan-salary-v3-jsonp';
 const CORE=['./','./index.html','./css/app.css','./js/api.js','./js/notifications.js','./js/app.js','./manifest.webmanifest','./assets/logo.svg','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
